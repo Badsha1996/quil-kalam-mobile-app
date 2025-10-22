7db2d6466b3a900287074df4cab52ad0b82ca50f
Badsha1996 <badshalaskar0@gmail.com>
2025-10-22 13:40:45 +0530

    prod

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,22 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const isEasBuild = process.env.EAS_BUILD === "true";
+
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+      // Keep NativeWind JSX source config
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
     ],
     plugins: [
-      "react-native-reanimated/plugin"
-    ]
+      // ⚡ Enable React Compiler only when NOT in EAS cloud
+      !isEasBuild && ["@react/compiler", { /* optional config */ }],
+
+      // 🌀 NativeWind (must come before Reanimated)
+      "nativewind/babel",
+
+      // 🎬 React Native Reanimated (must be last)
+      "react-native-reanimated/plugin",
+    ].filter(Boolean),
   };
 };
---
+++ nativewind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  input: "./app/global.css",
+  presets: [require("nativewind/preset")],
+};
